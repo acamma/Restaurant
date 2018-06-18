e7c2fa25c4cc31ebfab8882344429e8c31f9080e
acamma <40288873+acamma@users.noreply.github.com>
2018-06-18 12:21:59 +0200

    Add files via upload
    waiterModule

---
+++ waiterModule/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { OrdiniComponent } from './ordini/ordini.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    OrdiniComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ waiterModule/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoginUserData } from '../models/login-user-data';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  loginWaiterData = new LoginUserData();
+
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router
+  ) { }
+
+  loginWaiter() {
+    this.auth.loginWaiter(this.loginWaiterData).subscribe(
+      res => {
+        localStorage.setItem("waiterId", String(res.employeeId));
+        console.log(res);
+        this.router.navigate(["/ordini"]);
+      },
+      error => alert(error)
+    );
+  }
+}
